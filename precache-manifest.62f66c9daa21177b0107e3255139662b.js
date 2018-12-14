55da0f043fe3146f5703e71f6721251351ebfd51
Shukla, Priyavrat <priyavrat.shukla@bell.ca>
2018-12-14 14:23:09 +0530

    Updates

---
+++ precache-manifest.62f66c9daa21177b0107e3255139662b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dae516fa14f5d73818be",
+    "url": "/my-react-world/static/js/runtime~main.dae516fa.js"
+  },
+  {
+    "revision": "57fee48aad18de062d86",
+    "url": "/my-react-world/static/js/main.57fee48a.chunk.js"
+  },
+  {
+    "revision": "16c8866f5029132d614b",
+    "url": "/my-react-world/static/js/1.16c8866f.chunk.js"
+  },
+  {
+    "revision": "57fee48aad18de062d86",
+    "url": "/my-react-world/static/css/main.f81d2544.chunk.css"
+  },
+  {
+    "revision": "4eee114120ff58c7b15f217aa2681817",
+    "url": "/my-react-world/index.html"
+  }
+];
