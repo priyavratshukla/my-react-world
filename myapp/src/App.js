f359d9cbda7efd34b003e59725071e66bfa48bb3
Shukla, Priyavrat <priyavrat.shukla@bell.ca>
2018-12-14 13:34:19 +0530

    react app project commit

---
+++ myapp/src/App.js
@@ -0,0 +1,41 @@
+/*import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;*/
+
+import React from 'react';
+import './App.css';
+export default () => (
+  <div className="App">
+    <header className="App-header">
+      <h1 className="App-title">Welcome To My Hello World!</h1>
+    </header>
+    <p className="App-intro">
+      My name is Priyavrat. Welcome, and hello!
+    </p>
+  </div>
+);
