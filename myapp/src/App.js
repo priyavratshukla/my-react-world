73a776d0270c1b1c8ef14b4a88c00e119e6e3825
Shukla, Priyavrat <priyavrat.shukla@bell.ca>
2018-12-17 16:29:15 +0530

    test deploy

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -32,7 +32,7 @@ import './App.css';
 export default () => (
   <div className="App">
     <header className="App-header">
-      <h1 className="App-title">Bell canada</h1>
+      <h1 className="App-title">Bell canada </h1>
     </header>
     <p className="App-intro">
       Hi, I am TechM. Welcome, and hello!
